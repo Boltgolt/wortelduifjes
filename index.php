d8f111d9d4bf1bc0cc5d8fdfb31f55f2bed98f56
Jules Beuzenberg <203659@gmail.com>
2016-02-11 20:47:23 +0100

    Heb een beetje aan de HTML en de CSS gewerkt
    Was erg leuk

--- index.php
+++ index.php
@@ -6,8 +6,16 @@
 <html>
 <head>
 	<title><?= $globalTitle ?> - Home</title>
+	<link rel="stylesheet" type="text/css" href="index.css">
 </head>
 <body>
-	<a href="/register.php">Registreer</a>
+	<div id="header">
+		<ul>
+			<li><a href="/action/logIn">Log in</a></li>
+			<li><a href="/action/logIn">Registreer</a></li>
+			<li><a href="/overOns">Over ons</a></li>
+			<li><a href="/contact">Contact</a></li>
+		</ul>
+	</div>
 </body>
 </html>

8f1bba53fd558b84cab9aa14def573540ab05162
boltgolt <boltgolt@gmail.com>
2016-02-15 17:35:42 +0100

    Index klaar (negeer de header)

--- index.php
+++ index.php
@@ -9,14 +9,33 @@
 
 	<script type="text/javascript" src="static/script/skrollr.min.js"></script>
 	<script type="text/javascript">
-	window.addEventListener("load", function() {
-		var s = skrollr.init();
-	})
+		window.addEventListener("load", function() {
+			var s = skrollr.init();
 
+			var opp = ["worteltrekken", "differentiëren", "analyseren", "integreren"]
+			var elem = document.getElementById("opp")
+			var index = 0
+
+			setInterval(function () {
+				if (index < opp.length - 1) {
+					index++
+				}
+				else {
+					index = 0
+				}
+
+				elem.style.opacity = 0;
+
+				setTimeout(function () {
+					elem.innerHTML = opp[index];
+					elem.style.opacity = 1;
+				}, 310);
+			}, 3000);
+		})
 	</script>
 
 	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Open+Sans:400,300">
+	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Architects+Daughter|Open+Sans:400,300">
 	<link rel="stylesheet" type="text/css" href="static/style/global.css">
 	<link rel="stylesheet" type="text/css" href="static/style/index.css">
 </head>
@@ -33,21 +52,32 @@
 		<span>Zoek je <strong>exacte</strong> match</span>
 		<button>Registreer nu</button>
 	</div>
-	<div id="explanation" data-bottom-top="top: 100vh;" data-top-bottom="top: 30vh;">
-		<div data-100-bottom="opacity: 1" data-bottom-top="opacity: 0">
+	<div id="explanation">
+		<div data-200-bottom="opacity: 0" data--100-bottom="opacity: 1">
 			<img src="static/images/randomMath.png">
-			<p data-100-bottom="margin-top: 100px;" data-bottom-top="margin-top: 50px;">
+			<p data-200-bottom="margin-top: 50px;" data--300-bottom="margin-top: 170px;">
 				Op Wortelduifjes™ komen de leukeste wiskunde enthousiastelingen elkaar tegen
 			</p>
 		</div>
 	</div>
 	<div id="quote">
 		<div>
-			<p>
-				"Beste quote ooit"
+			<p data-200-bottom="opacity: 0" data--200-bottom="opacity: 1">
+				"Ik dacht dat de <strong>kans</strong> dat ik mijn wederhelft tegenkwam even <strong>reëel</strong> was als de <strong>wortel van -2</strong>, maar Wortelduifjes™ heeft het tegendeel bewezen."
 			</p>
-			<span>Henk Jansen</span>
+			<span  data-100-bottom="margin-right: 0px; opacity: 0;" data--200-bottom="margin-right: 55px; opacity: 1;">Gill Bates, Wiskundedocent</span>
 			<img src="static/images/person.png">
+		</div>
+	</div>
+	<div id="hurrytheforkup">
+		<div>
+			Stop met <span id="opp">worteltrekken</span>, <a href="/register.php">registreer nu</a>.
+		</div>
+	</div>
+	<div id="footer">
+		<div>
+			<span>©2015, Wortelduifjes™</span>
+			<span>Cookies</span>
 		</div>
 	</div>
 </body>

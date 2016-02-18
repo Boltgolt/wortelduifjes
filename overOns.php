531239e72082d00bb47576fbc8e8180383ed350d
emmaefm <emma.engels@hotmail.nl>
2016-02-18 17:43:26 +0100

    Create overOns.php

---
+++ overOns.php
@@ -0,0 +1,23 @@
+<?php
+	require "include/database.php";
+	require "include/strings.php";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title><?= $globalTitle ?> - Over ons</title>
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Architects+Daughter|Open+Sans:400,300">
+	<link rel="stylesheet" type="text/css" href="static/style/global.css">
+	<link rel="stylesheet" type="text/css" href="static/style/index.css">
+</head>
+<body>
+	<?php require "header.php" ?>
+	<p>
+	Op Wortelduifjes™ komen de leukeste wiskunde enthousiastelingen elkaar tegen. Hier hebben een aantal
+	mensen voor gezorgd. Ja, wij hebben er voor gezorgt dat u uw <strong>exacte</strong> match vindt of 
+	deze al gevonden heeft.
+	</p>
+
+</body>
+</html>

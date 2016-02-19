b52778508029fa89efc9a4b1e63df6d32b36b3bd
Jules Beuzenberg <203659@gmail.com>
2016-02-19 17:11:15 +0100

    Aan register en overOns gewerkt

--- overOns.php
+++ overOns.php
@@ -15,22 +15,40 @@
 	<?php require "header.php" ?>
 	<div>
 		<div id="lem">
-			<h1>Lem maria Hendriksen Severein</h1>
-			<h2>Opper oppperhoofd</h2>
+			<h1>Lem Maria Hendriksen Severein</h1>
+			<h2>Generaal</h2>
+			<h2>Javascript &#38; PHP</h2>
+			<p>Dit is een beschrijving van lem</p>
+			<img src="static/images/lem.jpg" alt="Lem Maria Hendriksen Severein">
 		</div>
 		<div id="leon">
 			<h1>Leon Ras</h1>
-			<h2>Medium opperhoofd</h2>
+			<h2>Kolonel</h2>
+			<h2>Javascript &#38; PHP</h2>
+			<p>Dit is een beschrijving van Leon</p>
+			<img src="static/images/leon.jpg" alt="Leon ras">
 		</div>
 	</div>
 	<div>
 		<div id="jules">
 			<h1>Julius Jesse Beuzenberg</h1>
-			<h2>Marginaal opperhoofd</h2>
+			<h2>Majoor</h2>
+			<h2>HTML &#38; CSS</h2>
+			<p>
+				Ik ben de HTML en CSS expert van deze website. Ik heb ruim &#233&#233n jaar aan ervaring 
+				met deze talen en border-radius is mijn expertise.
+			</p>
+			<p>
+				Dit is <strong>exact</strong> de <strong>functie</strong><br> die ik zocht bij een bedrijf.
+			</p>
+			<img src="static/images/jules.jpg" alt="Julius Jesse Beuzenberg">
 		</div>
 		<div id="emma">
-			<h1>Emma</h1>
-			<h2>Emma</h2>
+			<h1>Emma Engels</h1>
+			<h2>Kapitein</h2>
+			<h2>HTML &#38; CSS</h2>
+			<p>Emma</p>
+			<img src="static/images/emma.jpg" alt="Emma">
 		</div>
 	</div>
 </body>

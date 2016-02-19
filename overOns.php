ae9bfa5f05949091f1d1cf682a92e19e9ff3f73c
JuliusJesse <203659@gmail.com>
2016-02-19 12:36:30 +0100

    Html van overons verbeterd in elk opzicht

--- overOns.php
+++ overOns.php
@@ -9,20 +9,30 @@
 	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Architects+Daughter|Open+Sans:400,300">
 	<link rel="stylesheet" type="text/css" href="static/style/global.css">
-	<link rel="stylesheet" type="text/css" href="static/style/index.css">
+	<link rel="stylesheet" type="text/css" href="static/style/overOns.css">
 </head>
 <body>
 	<?php require "header.php" ?>
-	<div id="match gevonden">
-		<p>
-	Op Wortelduifjes™ komen de leukeste wiskunde enthousiastelingen elkaar tegen. Hier hebben een aantal
-	mensen voor gezorgd. Ja, wij hebben er voor gezorgt dat u uw <strong>exacte</strong> match vindt of 
-	deze al gevonden heeft.
-	</p>
+	<div>
+		<div id="lem">
+			<h1>Lem maria Hendriksen Severein</h1>
+			<h2>Opper oppperhoofd</h2>
+		</div>
+		<div id="leon">
+			<h1>Leon Ras</h1>
+			<h2>Medium opperhoofd</h2>
+		</div>
 	</div>
-
-	<p> Ons team bestaat uit 4 enthausiaste leerlingen die een datingsite moesten maken voor school. Wij
-	zijn: Lem, Jules, Leon en Emma en helpen u graag met het zoeken naar de <strong>exacte</strong> match!
-	</p>
+	<div>
+		<div id="jules">
+			<h1>Julius Jesse Beuzenberg</h1>
+			<h2>Marginaal opperhoofd</h2>
+		</div>
+		<div id="emma">
+			<h1>Emma</h1>
+			<h2>Emma</h2>
+		</div>
+	</div>
 </body>
 </html>
+

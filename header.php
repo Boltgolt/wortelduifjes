8b3152fde5a3ad388144fae72648c878595faf89
boltgolt <boltgolt@gmail.com>
2016-02-20 13:02:31 +0100

    Fixes en header.php

---
+++ header.php
@@ -0,0 +1,14 @@
+<div id="header">
+	<img src="static/images/logo.png">
+	<ul>
+		<a href="/register.php"><li>Registreer</li></a>
+		<a href="/overOns.php"><li>Over ons</li></a>
+		<a href="/contact.php"><li>Contact</li></a>
+	</ul>
+	<a href="#">
+		<div id="headerUser">
+			<img src="static/images/noPhoto.png">
+			<span>Log in</span>
+		</div>
+	</a>
+</div>

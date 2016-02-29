a1230abd55d01ebe6f023178f556d8ef95bff541
Leon203672 <superleonbros@hotmail.com>
2016-02-29 10:11:15 +0100

    Update header.php

--- include/header.php
+++ include/header.php
@@ -1,3 +1,18 @@
+<?php
+	if(!empty($_SESSION["id"]) {
+		require "include/database.php";
+		require "include/strings.php";
+		$query = mysqli_query($mysqli, "SELECT * FROM users WHERE id='" . mysqli_escape_string($mysqli, $_SESSION["id"]) . "'");
+		if (!$query) {
+			die("MySQL fout");
+		}
+		if (mysqli_num_rows($query) == 0) {
+			die("Niet bestaand profiel");
+			// TODO: niet die pleas
+		}
+		$user = mysqli_fetch_assoc($query)
+	}
+?>
 <div id="header">
 	<a href="/">
 		<img src="static/images/logo.png">
@@ -7,10 +22,20 @@
 		<a href="/overOns.php"><li>Over ons</li></a>
 		<a href="/contact.php"><li>Contact</li></a>
 	</ul>
+	<?php if(!empty($_SESSION["id"])): ?>
 	<a href="#">
+		<div id="headerUser">
+			<img src="/photos/<?= $user["id"] ?>">
+			<span><?= $user["firstName"]?> <?= $user["lastName"]?></span>
+		</div>
+	</a>
+	<?php else: ?>
+		<a href="#">
 		<div id="headerUser">
 			<img src="static/images/noPhoto.png">
 			<span>Log in</span>
 		</div>
 	</a>
+	<?php endif; ?>
 </div>
+

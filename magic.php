f0eee50705136c897e3e977b6d672450b9a34a2f
Leon203672 <superleonbros@hotmail.com>
2016-03-08 10:51:38 +0100

    Create magic.php

---
+++ magic.php
@@ -0,0 +1,31 @@
+<?php
+	require "include/database.php";
+	require "include/strings.php";
+	session_start();
+	if (empty($_SESSION["id"])) {
+	  die();
+  }
+  $query = mysqli_query($mysqli, "SELECT * FROM users ORDER BY RAND() LIMIT 1");
+	if (!$query) {
+		die("MySQL fout");
+	}
+	if (mysqli_num_rows($query) == 0) {
+		die("Niet bestaand profiel");
+	}
+	$user = mysqli_fetch_assoc($query)
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title><?= $globalTitle ?> - Wellicht is dit de goede y?</title>
+
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Architects+Daughter|Open+Sans:400,300">
+	<link rel="stylesheet" type="text/css" href="static/style/global.css">
+	<link rel="stylesheet" type="text/css" href="static/style/profile.css">
+</head>
+<body>
+	<?php require "include/header.php" ?>
+	<?= $user["firstName"] ?>
+</body>
+</html>

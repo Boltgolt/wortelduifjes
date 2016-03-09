8d6d2d0976c807d9bf69aeb601f0b9568dad8528
Leon203672 <superleonbros@hotmail.com>
2016-03-09 10:11:00 +0100

    Update delete.php

--- actions/delete.php
+++ actions/delete.php
@@ -3,5 +3,9 @@
 	require "../include/strings.php";
 	session_start();
 	
+	if (empty($_SESSION["id"])) {
+		die();
+	}
+	
 	$query = mysqli_query($mysqli, "DELETE FROM users WHERE id=" . $_SESSION["id"]);
 ?>
